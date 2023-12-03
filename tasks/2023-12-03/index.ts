e38db460230268335cbe520a0d64daf11233893c
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-12-03 19:02:36 +0100

    Task 03.12.2023 finished

--- tasks/2023-12-03/index.ts
+++ tasks/2023-12-03/index.ts
@@ -5,20 +5,17 @@ export interface Lokalizacja {
   czas: number;
 }
 
-export type MapaCzasoprzestrzenna = (
-  x: number,
-  y: number,
-  z: number,
-  czas: number
-) => number;
+export type MapaCzasoprzestrzenna = (x: number, y: number, z: number, czas: number) => number;
 
-export const znajdzWorek = (
-  lokalizacje: Lokalizacja[],
-  mapa: MapaCzasoprzestrzenna
-): Lokalizacja | null | undefined => {
-  if (lokalizacje.length === 0) {
+export const znajdzWorek = (lokalizacje: Lokalizacja[], mapa: MapaCzasoprzestrzenna): Lokalizacja | null | undefined => {
+  if (lokalizacje.length === 0 || lokalizacje.some((lok) => Object.values(lok).some((val) => typeof val !== "number"))) {
     return null;
   }
-  const arr = [];
-  lokalizacje.map((lok) => mapa(lok.x, lok.y, lok.z, lok.czas));
+
+  const mappedLok: number[] = lokalizacje.map((lok) => mapa(lok.x, lok.y, lok.z, lok.czas));
+  if (mappedLok.some((val) => Number.isNaN(val))) {
+    return null;
+  }
+  const indexOfMax: number = mappedLok.indexOf(Math.max(...mappedLok));
+  return lokalizacje[indexOfMax];
 };
